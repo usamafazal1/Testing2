569ae2e26a2db778bb9e4e1799d16799369753f5
Usama Fazal <Usama.Fazal@outlook.com>
2022-07-25 06:46:49 -0400

    Add project files.

---
+++ Task21.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Task21.aspx.cs" Inherits="Testing2.Task21" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 195px">
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+        <p>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" Text="Restore with View State" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" runat="server" Text="Restore with Hidden Fields" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Task21.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Testing2
+{
+    public partial class Task21 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //when page is loaded, both entered values are stored in 'name' and 'password'//
+            if (IsPostBack)
+            {
+                ViewState["name"] = TextBox1.Text;
+                ViewState["password"] = TextBox1.Text;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                TextBox1.Text = "";
+                TextBox2.Text = "";
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            { 
+                TextBox1.Text = ViewState["name"].ToString();
+                TextBox2.Text = ViewState["password"].ToString();
+            }
+
+        }
+    }
+}
